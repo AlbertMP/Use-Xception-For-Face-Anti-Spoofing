52cf5d1310e5519939f4cede3af34c1877386272
Peng Miao <miaomiao315@hotmail.com>
2021-03-09 12:03:12 +0800

    Update flask-inference.py
    add inference server

---
+++ flask_api/flask-inference.py
@@ -0,0 +1,64 @@
+import numpy as np
+from keras.applications.xception import preprocess_input
+from keras.preprocessing import image
+from keras.models import load_model
+
+# flask configuration
+from flask import request, Flask, jsonify
+from matplotlib import pyplot as plt
+app = Flask(__name__)
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'bmp'])
+
+# optimize the utilization of 3090 GPU accelerator
+import tensorflow as tf
+config = tf.compat.v1.ConfigProto(allow_soft_placement=False, log_device_placement=False)
+config.gpu_options.allow_growth = True
+session = tf.compat.v1.Session(config=config)
+
+# load model
+model = load_model("../result-balanced-4w_10_100_180_16_1e-3_1e-4_1/model_fine_final.h5")
+# load class names
+with open("../classes.txt", 'r') as f:
+    classes = list(map(lambda x: x.strip(), f.readlines()))
+
+@app.route('/inference/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # 使用request上传文件，其中‘file'表示前端表单的Key值；也可以使用request.files['file']
+        f = request.files.get('file')
+        # 判断是否上传成功
+        if f is None:
+            return jsonify({"Code": "401", "Info": "no file uploaded"})
+        # 检查文件后缀名是否是图片文件
+        if not allow_file(f.filename):
+            return jsonify({"Code": "402", "Info": "file not supported, support jpg, jpeg, png"})
+        # read img as numpy array, remember to
+        np_img = plt.imread(f) * 255
+        # 对图片进行处理
+        # Your code
+
+        # load an input image
+        # img = image.load_img(args.image, target_size=(299, 299))
+        x = image.img_to_array(np_img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        # predict
+        pred = model.predict(x)[0]
+        result = [(classes[i], float(pred[i]) * 100.0) for i in range(len(pred))]
+        result.sort(reverse=True, key=lambda x: x[1])
+        for i in range(2):
+            (class_name, prob) = result[i]
+            print("Top %d ====================" % (i + 1))
+            print("Class name: %s" % (class_name))
+            print("Probability: %.2f%%" % (prob))
+        result = dict(result)
+    return jsonify({"Code": "200", "Info": result})
+
+
+# 检查文件后缀名是否是图片文件
+def allow_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+if __name__ == '__main__':
+    app.run()
